bad402c6d66f7adb0c2a1a1ffddcbecbc715dc72
Diaboy01 <marvin.prigenitz@gmx.de>
2024-02-06 20:08:19 +0100

    login and logout now works

--- src/auth.js
+++ src/auth.js
@@ -20,7 +20,6 @@ const dbConfig = checkForDevArg() ? {
   database: 'zwitschern'
 };
 
-
 const config = {
     authRequired: false,
     auth0Logout: true,
@@ -31,67 +30,81 @@ const config = {
   };
   const port = 5000;
 
-  // auth router attaches /login, /logout, and /callback routes to the baseURL
-  app.use(auth(config));
+app.use(auth(config));
 
-  // Funktion zum Überprüfen von Command-Line-Argumenten
+// Funktion zum Überprüfen von Command-Line-Argumenten
 function checkForDevArg() {
   return process.argv.includes('-dev'); // Nutze den Befehl "node app.js -dev" um die lokale Datenbank zu verwenden
 }
-  
-  // req.isAuthenticated is provided from the auth router
-  app.get('/auth/check', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
-  
-  app.get('/auth/profile', requiresAuth(), async (req, res) => {
-    const userData = {
-      email: req.oidc.user.email,
-      username: req.oidc.user.nickname
-    };
-  
-    try {
-      const connection = await mysql.createConnection(dbConfig);
-  
-      // Der SQL-Befehl prüft, ob ein Benutzer mit der gleichen E-Mail existiert und aktualisiert dessen Benutzernamen.
-      // Falls der Benutzer nicht existiert, wird ein neuer Eintrag erstellt.
-      const sql = `
-        INSERT INTO user (email, username) 
-        VALUES (?, ?) 
-        ON DUPLICATE KEY UPDATE username = VALUES(username);
-      `;
-      await connection.execute(sql, [userData.email, userData.username]);
-  
-      await connection.end();
-  
-      res.send(JSON.stringify(userData));
-    } catch (error) {
-      console.error('Fehler beim Speichern des Nutzers: ', error.message);
-      res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
-    }
-  });
-  
 
-  app.get('/auth/callback', (req, res) => {
-    res.oidc.callback({
-      redirectUri: 'http://localhost:3000/callback',
-    });
+// login and redirect to account
+app.get('/auth/login', requiresAuth(), (req, res) => {
+    res.redirect('/account');
   });
   
-  app.post('/auth/callback', express.urlencoded({ extended: false }), (req, res) =>
-    res.oidc.callback({
-      redirectUri: 'http://localhost:3000/callback',
-    }
-    ));
+// logout and redirect to home
+app.get('/auth/logout', (req, res) => {
+  res.oidc.logout({
+    returnTo: 'https://zwitschern.chat',
+  });
+});
+
+// check if user is logged in or not
+app.get('/auth/check', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+// profile and send user data to extra database 
+app.get('/auth/profile', requiresAuth(), async (req, res) => {
+  const userData = {
+    email: req.oidc.user.email,
+    username: req.oidc.user.nickname
+  };
+
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    // Der SQL-Befehl prüft, ob ein Benutzer mit der gleichen E-Mail existiert und aktualisiert dessen Benutzernamen.
+    // Falls der Benutzer nicht existiert, wird ein neuer Eintrag erstellt.
+    const sql = `
+      INSERT INTO user (email, username) 
+      VALUES (?, ?) 
+      ON DUPLICATE KEY UPDATE username = VALUES(username);
+    `;
+    await connection.execute(sql, [userData.email, userData.username]);
+
+    await connection.end();
+
+    res.send(JSON.stringify(userData));
+  } catch (error) {
+    console.error('Fehler beim Speichern des Nutzers: ', error.message);
+    res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
+  }
+});
+
+// callback get
+app.get('/auth/callback', (req, res) => {
+  res.oidc.callback({
+    redirectUri: 'http://localhost:3000/callback',
+  });
+});
+
+// callback post 
+app.post('/auth/callback', express.urlencoded({ extended: false }), (req, res) =>
+res.oidc.callback({
+  redirectUri: 'http://localhost:3000/callback',
+  }
+));
     
-  
-    app.listen(port, () => {
-      // Gibt eine Meldung aus, welche Konfiguration verwendet wird
-      if (checkForDevArg()) {
-        console.log(`Entwicklungsmodus aktiviert: AUTH hört auf Port ${port} (lokal)`);
-      } else {
-        console.log(`Produktionsmodus aktiviert: AUTH hört auf Port ${port} (live)`);
-      }
-    });
+// start server  
+app.listen(port, () => {
+  // Gibt eine Meldung aus, welche Konfiguration verwendet wird
+  if (checkForDevArg()) {
+    console.log(`Entwicklungsmodus aktiviert: AUTH hört auf Port ${port} (lokal)`);
+  } else {
+    console.log(`Produktionsmodus aktiviert: AUTH hört auf Port ${port} (live)`);
+  }
+});
 
   
