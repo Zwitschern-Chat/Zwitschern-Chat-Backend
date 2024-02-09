aca32b015c07ca926cd7a28a79dc079010487011
Diaboy01 <marvin.prigenitz@gmx.de>
2024-02-09 15:37:11 +0100

    const sanitizeHtml = require('sanitize-html');

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const app = express();
 const cors = require('cors');
+const sanitizeHtml = require('sanitize-html');
 
 const corsOptions = {
   origin: '*', 
