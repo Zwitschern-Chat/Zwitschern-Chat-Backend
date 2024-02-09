43fb5b7acad21b201311936a03a344bc55a2d057
Diaboy01 <marvin.prigenitz@gmx.de>
2024-02-09 16:48:18 +0100

    Added & updated api endpoints

--- src/app.js
+++ src/app.js
@@ -42,25 +42,6 @@ const dbConfig = checkForDevArg() ? {
 };
 
 
-// Endpoint zum Abrufen eines Posts anhand der Post-ID
-app.get('/api/post/:id', async (req, res) => {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows, fields] = await connection.execute('SELECT * FROM post WHERE id = ?;', [req.params.id]);
-    await connection.end();
-
-    if (rows.length === 0) {
-      res.status(404).send('Post nicht gefunden.');
-    } else {
-      res.json(rows[0]);
-    }
-  } catch (error) {
-    console.error('Fehler beim Zugriff auf die Datenbank: ', error.message);
-    res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
-  }
-});
-
-
 // Endpoint zum Abrufen aller Posts
 app.get('/api/posts', async (req, res) => {
   try {
@@ -81,6 +62,25 @@ app.get('/api/posts', async (req, res) => {
     res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
   }
 });
+
+// Endpoint zum Abrufen eines Posts anhand der Post-ID
+app.get('/api/post/:id', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows, fields] = await connection.execute('SELECT * FROM post WHERE id = ?;', [req.params.id]);
+    await connection.end();
+
+    if (rows.length === 0) {
+      res.status(404).send('Post nicht gefunden.');
+    } else {
+      res.json(rows[0]);
+    }
+  } catch (error) {
+    console.error('Fehler beim Zugriff auf die Datenbank: ', error.message);
+    res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
+  }
+});
+
 
 // Endpoint zum Abrufen aller Nutzer (geheime Daten wie sub)
 app.get('/api/users', async (req, res) => {
@@ -151,6 +151,30 @@ app.get('/api/user_sub/:sub', async (req, res) => {
 
 
 // Endpoint zum Abrufen aller Posts eines Nutzers
+app.get('/api/user_posts/:number', async (req, res) => {
+  let { number } = req.params;
+
+  // To prevent sql injection, xss and other attacks here use sanitize-html package
+  number = sanitizeHtml(number, {
+    allowedTags: [],
+    allowedAttributes: {}
+  });
+
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows, fields] = await connection.execute('SELECT * FROM post WHERE user_number = ?;', [number]);
+    await connection.end();
+
+    if (rows.length === 0) {
+      res.status(404).send('Keine Posts gefunden.');
+    } else {
+      res.json(rows);
+    }
+  } catch (error) {
+    console.error('Fehler beim Zugriff auf die Datenbank: ', error.message);
+    res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
+  }
+});
 
 
 app.listen(port, () => {
