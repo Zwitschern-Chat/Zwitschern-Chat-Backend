97477942b74c57301ab67655f255d999e3b55867
Diaboy01 <marvin.prigenitz@gmx.de>
2024-02-09 17:27:51 +0100

    Cleaned up auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,4 @@
+// Imports
 const express = require('express');
 const app = express();
 const { auth, requiresAuth } = require('express-openid-connect');
@@ -24,7 +25,7 @@ const dbConfig = checkForDevArg() ? {
 const config = {
     authRequired: false,
     auth0Logout: true,
-    secret: process.env.SECRET,   //secrect import from .env file
+    secret: process.env.SECRET,   // Secrect import from .env file
     baseURL: 'https://zwitschern.chat/auth',
     clientID: '6dHWgZ691XG9pTlUar21zBrdVEvctyFP',
     issuerBaseURL: 'https://dev-x6a4ln1r3kk4uz5p.us.auth0.com'
@@ -62,29 +63,29 @@ app.use(async (req, res, next) => {
         // User already exists, optionally update the username and profile picture
         const updateSql = `UPDATE user SET username = ?, profile_picture = ? WHERE sub = ?;`;
         await connection.execute(updateSql, [userData.username, userData.profile_picture, userData.sub]);
-        console.log('Benutzerdaten aktualisiert.');
+        console.log('Userdata updated.');
       } else {
         // User does not exist, insert the new user
         const insertSql = `INSERT INTO user (sub, username, profile_picture) VALUES (?, ?, ?);`;
         await connection.execute(insertSql, [userData.sub, userData.username, userData.profile_picture]);
-        console.log('Neuer Benutzer hinzugefügt.');
+        console.log('New user created.');
       }
 
       await connection.end();
     } catch (error) {
-      console.error('Fehler beim Aktualisieren der Benutzerdaten in der Datenbank: ', error.message);
+      console.error('Error accessing the database: ', error.message);
     }
   }
   next();
 });
 
 
-// Endpoint zum Erstellen eines neuen Posts
+// Endpoint to create a new post as chat message from a user
 app.post('/auth/post', requiresAuth(), async (req, res) => {
   const sub = req.oidc.user.sub;
   let { message, user_number } = req.body; // Get data from the request body
 
-    // Do prevent sql injection, xss and other attacks here use sanitize-html package  
+    // Prevent sql injection, xss and other attacks here use sanitize-html package  
     message = sanitizeHtml(message, {
       allowedTags: [],
       allowedAttributes: {}
@@ -94,82 +95,80 @@ app.post('/auth/post', requiresAuth(), async (req, res) => {
       allowedAttributes: {}
     });
 
-  // use sub for to get number to compare with user_number
+  // Use sub for to get number to compare with user_number
   try {
     const connection = await mysql.createConnection(dbConfig);
     const [rows, fields] = await connection.execute('SELECT number FROM user WHERE sub = ?;', [sub]);
     await connection.end();
 
     if (rows.length === 0) {
-      res.status(404).send('Benutzer nicht gefunden.');
+      res.status(404).send('User not found.');
     } else {
       number = rows[0].number;
     }
   } catch (error) {
-    console.error('Fehler beim Zugriff auf die Datenbank: ', error.message);
-    res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
+    console.error('Error accessing the database: ', error.message);
+    res.status(500).send('Error accessing the database: ', error.message);
   }
 
   if (user_number != number) {
-    return res.status(403).send('Nicht autorisiert: Sie können nur Posts für Ihren eigenen Benutzer erstellen! ' + user_number + '!=' + number);
+    return res.status(403).send('Not authorized! ' + user_number + '!=' + number);
   }
 
   try {
     const connection = await mysql.createConnection(dbConfig);
     const [rows, fields] = await connection.execute('INSERT INTO post (message, user_number) VALUES (?, ?);', [message, user_number]);
     await connection.end();
-    res.json({ success: true, message: 'Post erfolgreich erstellt.', postId: rows.insertId });
+    res.json({ success: true, message: 'Post created successfully.', postId: rows.insertId });
   } catch (error) {
-    console.error('Fehler beim Zugriff auf die Datenbank: ', error.message);
-    res.status(500).send('Fehler beim Zugriff auf die Datenbank: ' + error.message);
+    console.error('Error accessing the database: ', error.message);
+    res.status(500).send('Error accessing the database: ', error.message);
   }
 });
 
 
-
-
-// endpoint to get all user data from auth0 
+// Endpoint to get all user data from auth0 
 app.get('/auth/user', requiresAuth(), (req, res) => {
   res.json(req.oidc.user);
 });
 
-// login
+// Login and redirect to home
 app.get('/auth/login', requiresAuth(), (req, res) => {
-    res.redirect('/account');
+  res.redirect('https://zwitschern.chat');
   });
 
-// account page 
+// Account and redirect to account
 app.get('/auth/account', requiresAuth(), (req, res) => {
-  res.redirect('https://zwitschern.chat');
+  res.redirect('/account');
 });
   
-// logout and redirect to home
+// Logout and redirect to home
 app.get('/auth/logout', (req, res) => {
   res.oidc.logout({
     returnTo: 'https://zwitschern.chat',
   });
 });
 
-// debug, check if user is logged in or not
+// Only to debug: Check if user is logged in or not
 app.get('/auth/check', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-// callback get
+// Callback get for auth0 needed
 app.get('/auth/callback', (req, res) => {
   res.oidc.callback({
     redirectUri: 'http://localhost:3000/callback',
   });
 });
 
-// callback post 
+// Callback post for auth0 needed
 app.post('/auth/callback', express.urlencoded({ extended: false }), (req, res) =>
 res.oidc.callback({
   redirectUri: 'http://localhost:3000/callback',
   }
 ));
     
-// start server  
+// Start the server
 app.listen(port, () => {
   // Outputs a message indicating which configuration is being used
   if (checkForDevArg()) {
